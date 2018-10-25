11e7ac838a2be6b1b98303205b364080f5c9e860
Yury Kirilyuk <yury@cyberiansoft.com>
2018-10-25 19:44:49 +0300

    modals

--- app/js/custom.js
+++ app/js/custom.js
@@ -117,15 +117,32 @@ $(function(){
     $( "#value" ).text( $( "#slider" ).slider( "value" ) + "$");
 
 
+
+
     $('.grid').masonry({
         itemSelector: '.project',
         fitWidth: true,
         gutter: 30
     });
 
+
+
+
 });
 
 
+$(".button-file input").change(function() {
+
+    var getPath = $(this).val();
+
+    function getName (str){
+        if (str.lastIndexOf('\\')){var i = str.lastIndexOf('\\')+1;}
+        else{var i = str.lastIndexOf('/')+1;}
+        var filename = str.slice(i);
+        $("#fileformlabel").text(filename);
+    }
+    getName(getPath);
+});
 
 function headerColor() {
     var headerOffset = $('header').offset().top;
